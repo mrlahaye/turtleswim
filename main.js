88c58e07064968a35a2bc34df9452e3ce24bfd52
mrlahaye <42346760+mrlahaye@users.noreply.github.com>
2018-11-12 13:59:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ var mainState={
         //This function will be executed at game start
         //Location to load ressources like images/sounds
         game.load.image('bird', 'assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
     },
     create: function(){
+        //Initialize Score
+        this.score=0;
+        this.labelScore = game.add.text(20, 20, "0",
+        { font: "30px Arial", fill: "#ffffff" });
+        
         //This function is called after the preload one
         //Location to setup the game, sprites...
         game.stage.backgroundColor = '#71c5cf';
@@ -27,7 +33,13 @@ var mainState={
         //Make the turtleswim on spacebar hit
         var spaceKey = game.input.keyboard.addKey(
                         Phaser.Keyboard.SPACEBAR);
-        spaceKey.onDown.add(this.jump, this);        
+        spaceKey.onDown.add(this.jump, this);    
+        
+        //Create a group to display all pipes
+        this.pipes = game.add.group();
+        
+        //Create a timer to display the pipes
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
         
     },
     update: function(){
@@ -37,6 +49,8 @@ var mainState={
       //If the turtle is out of the screen, restart game
       if (this.bird.y < 0 || this.bird.y > 490)
           this.restartGame();
+      game.physics.arcade.overlap(
+              this.bird, this.pipes, this.restartGame, null, this);
     },
     jump: function(){
         //Add a Vertical velocity to the turtle
@@ -45,6 +59,35 @@ var mainState={
     restartGame: function(){
         //Start the 'Main' state, restarting the game
         game.state.start('main');
+    },
+    addOnePipe: function(x,y){
+        //Create a pipe at x,y postion
+        var pipe = game.add.sprite(x, y, 'pipe');
+        
+        //Add the pipe to the group in the create function
+        this.pipes.add(pipe);
+        
+        //Enable physics on the pipes
+        game.physics.arcade.enable(pipe);
+        
+        //Adding some velocity to the pipes to make em move
+        pipe.body.velocity.x =-200;
+        
+        //Automatically remove pipes when no longer visible
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;        
+    },
+    addRowOfPipes: function(){
+        //Randomly pick a number between 1 to 5 
+        var hole = Math.floor(Math.random() * 5)+ 1;
+        
+        //Add 6 Pipes
+        //With an hole  at position 'hole' and 'hole+1'
+        for(var i = 0; i<8; i++)
+            if(i != hole && i != hole + 1)
+                this.addOnePipe(400,i*60+10);
+        this.score += 1;
+        this.labelScore.text = this.score;
     }
 };
 //Initializing Phaser module and create "playspace"
@@ -54,4 +97,4 @@ var game = new Phaser.Game(400,490);
 game.state.add('main',mainState);
 
 //Starting the game
-game.state.start('main');
+game.state.start('main');    
